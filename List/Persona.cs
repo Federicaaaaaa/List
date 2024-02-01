e8862a730cff02b0c6af22df9a0f4174ed18fcc2
Federicaaaaaa <danger2006.bona@gmail.com>
2024-02-01 15:43:40 +0100

    Aggiungere i file di progetto.

---
+++ List/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+
+        public override string ToString()
+        {
+            return $"Persona di nome {Nome} che ha {Eta}";
+        }
+    }
+}
